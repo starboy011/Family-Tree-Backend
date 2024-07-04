e508327da220fb190d3dc95283e3c60a833710fa
starboy011 <rishavofficial294@gmail.com>
2024-07-04 18:02:57 +0100

    added endpoint to get names

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/starboy011/Family-Tree-Backend
+
+go 1.22.3
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/starboy011/Family-Tree-Backend/internal/handler"
+)
 
 func main() {
-	fmt.Printf("Rishav")
+	// Define HTTP route for /get-all-names endpoint
+	http.HandleFunc("/get-all-names", handler.GetAllName)
+
+	// Start HTTP server on port 8080
+	fmt.Println("Starting server on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
