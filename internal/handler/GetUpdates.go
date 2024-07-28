6e02f7a4f33510501c1411aa6c5fa4489087a4ff
starboy011 <rishavofficial294@gmail.com>
2024-07-28 17:13:34 +0100

    added time logic in the backend

--- internal/handler/GetUpdates.go
+++ internal/handler/GetUpdates.go
@@ -2,10 +2,12 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/starboy011/Family-Tree-Backend/internal/db"
 )
 
@@ -15,6 +17,21 @@ type Updates struct {
 }
 
 func GetUpdates(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	currentTime := vars["currenttime"]
+	fmt.Println(currentTime)
+
+	// Parse the currentTime to time.Time
+	currentDate, err := time.Parse("2006-01-02", currentTime)
+	if err != nil {
+		http.Error(w, "Invalid date format", http.StatusBadRequest)
+		log.Fatalf("Invalid date format: %v", err)
+		return
+	}
+
+	// Calculate the date one week before the currentDate
+	oneWeekBefore := currentDate.AddDate(0, 0, -7)
+
 	db, err := db.InitDb(w, r)
 	if err != nil {
 		http.Error(w, "Error in connecting to db", http.StatusInternalServerError)
@@ -22,16 +39,18 @@ func GetUpdates(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer db.Close()
-	query := `SELECT "message", "time" FROM "Updates"`
 
-	// Execute query
-	rows, err := db.Query(query)
+	query := `SELECT "message", "time" FROM "Updates" WHERE "time" BETWEEN $1 AND $2`
+
+	// Execute query with date range parameters
+	rows, err := db.Query(query, oneWeekBefore, currentDate)
 	if err != nil {
 		http.Error(w, "Error executing query", http.StatusInternalServerError)
 		log.Fatalf("Error executing query: %v", err)
 		return
 	}
 	defer rows.Close()
+
 	var results []Updates
 
 	for rows.Next() {
@@ -55,7 +74,19 @@ func GetUpdates(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("Error iterating through rows: %v", err)
 		return
 	}
-
+	if len(results) == 0 {
+		noUpdatesMessage := map[string]string{"message": "No updates"}
+		jsonData, err := json.Marshal(noUpdatesMessage)
+		if err != nil {
+			http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
+			log.Fatalf("Error marshalling JSON: %v", err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonData)
+		return
+	}
 	// Convert results slice to JSON
 	jsonData, err := json.Marshal(results)
 	if err != nil {
